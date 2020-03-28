db3dfb663504eab6405b9db24712a3586973577f
Michael Hamilton <michaelhamilton626@gmail.com>
2020-03-28 01:59:07 -0400

    adds charcoal font, window component wip

--- os9/desktop/index.js
+++ os9/desktop/index.js
@@ -2,11 +2,13 @@
 import React, {Component} from 'react';
 import './styles.sass';
 import * as preferences from '../../preferences';
+import Window from "../window";
 
 class Desktop extends Component {
   render() {
     return (
       <div className='desktop' style={{backgroundColor: preferences.desktop.background}}>
+        <Window />
         <div className='icon'>
           <img className='icon-image'/>
           <p className='icon-label'>
--- os9/window/index.js
+++ os9/window/index.js
@@ -1,21 +1,53 @@
-// OS 9 Desktop
+// OS 9 Window
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import './styles.sass';
-import * as preferences from '../../preferences';
 
 class Window extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isMoving: false,
+      height: 400,
+      x: 0,
+      y: 0,
+      width: 400,
+    };
+  }
+
+
   render() {
     return (
-      <div className='desktop' style={{backgroundColor: preferences.desktop.background}}>
-        <div className='icon'>
-          <img className='icon-image'/>
-          <p className='icon-label'>
-            Icon
-          </p>
+      <div
+        className='window'
+        onMouseDown={() => this.setState({isMoving: true})}
+        onMouseUp={() => this.setState({isMoving: false})}
+        onMouseLeave={() => this.setState({isMoving: false})}
+        onMouseMove={(e) => {
+          if(this.state.isMoving) {
+            this.setState({x: e.clientX, y: e.pageY})
+          }
+        }}
+        style={{
+          height: this.state.height,
+          left: this.state.x,
+          top: this.state.y,
+          width: this.state.width,
+        }}
+      >
+        <div className='header'>
+          <label className='title'>{this.props.title}</label>
+        </div>
+        <div className='content'>
         </div>
       </div>
     );
   }
 }
+
+Window.defaultProps = {
+  title: 'Window'
+};
 
 export default Window;

1aa281c6ed8acd8104d446f17247e3c574ab1b6c
Michael Hamilton <michaelhamilton626@gmail.com>
2020-03-27 20:09:13 -0400

    menu render wip

--- os9/menu/index.js
+++ os9/menu/index.js
@@ -3,21 +3,37 @@ import React, {Component} from 'react';
 import './styles.sass';
 
 class Menu extends Component {
+
+  renderMenuItems(menuItems) {
+    return menuItems.map(menuItem => {
+      if(menuItem.items) {
+
+      }
+      return <li>{menuItem.name}</li>
+    })
+  }
+
   render() {
     return (
       <menu className='menu-bar'>
         <ul className='align-left'>
-          <li>File</li>
           <ul className='submenu'>
             <li>About This Computer</li>
             <li className='menu-separator' />
             <li>Menu 1</li>
           </ul>
-          <li>Edit</li>
-          <li>View</li>
-          <li>Window</li>
-          <li>Special</li>
-          <li>Help</li>
+          {
+            this.renderMenuItems([
+              {name: 'File', items: [
+                  {name: 'Save'}
+              ]},
+              {name: 'Edit'},
+              {name: 'View'},
+              {name: 'Window'},
+              {name: 'Special'},
+              {name: 'Help'},
+            ])
+          }
         </ul>
         <ul className='align-right'>
           <li className='no-hover'>12:00 PM</li>

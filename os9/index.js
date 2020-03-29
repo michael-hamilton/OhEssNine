ba7eecc1f7d6cc96445e870ed95e08dd7285a164
Michael Hamilton <michaelhamilton626@gmail.com>
2020-03-29 00:29:16 -0400

    adds startup loader

--- os9/index.js
+++ os9/index.js
@@ -3,9 +3,11 @@
 import React, {Fragment} from 'react';
 import Desktop from './desktop';
 import Menu from './menu';
+import Startup from './startup';
 
 const OS9 = () => (
   <Fragment>
+    <Startup />
     <Menu />
     <Desktop />
   </Fragment>
---
+++ os9/startup/index.js
@@ -0,0 +1,64 @@
+// OS 9 Startup
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import Cookies from 'js-cookie';
+import MikeOS from '../resources/images/mikeos.jpg';
+import './styles.sass';
+
+const Loader = (props) => (
+  <div className='loader'>
+    <label className='loader-label'>Starting Up...</label>
+    <div className='loading-bar'>
+      <div className='loading-progress' style={{width: `${props.progress}%`}} />
+    </div>
+  </div>
+);
+
+class Startup extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loaderProgress: 0
+    };
+  }
+
+  componentDidMount() {
+    if(Cookies.get('hasLoadedBefore')) {
+      this.setState({loaderProgress: 75});
+    }
+    this.initPseudoLoader();
+    Cookies.set('hasLoadedBefore', true);
+  }
+
+  // Updates the pseudo loader to simulate a loading bar
+  initPseudoLoader(){
+    let updateInterval;
+    const update = (u, t) => {
+      this.setState({loaderProgress: this.state.loaderProgress + u});
+      if(this.state.loaderProgress >= 100) {
+        clearInterval(updateInterval);
+      }
+    };
+    updateInterval = setInterval(() => update(1), 50);
+  }
+
+  render() {
+    return (
+      <div className='startup-wrapper' style={{display: this.state.loaderProgress >= 100 ? 'none' : 'flex'}}>
+        <div className='startup'>
+          <div className='content'>
+            <img src={MikeOS} height={140} width={140}/>
+            <h1 className='startup-title'>Mike OS 9</h1>
+          </div>
+          <Loader progress={this.state.loaderProgress}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+Startup.defaultProps = {
+};
+
+export default Startup;
